c70e0c891a9338bac228d9ac1d6870a07bf37b76
hsvidya10 <vidyavidya1776@gmail.com>
2024-10-16 00:24:23 -0700

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3]
+plt.scatter(x,y,color= 'green')
+plt.xlabel("xaxis",fontsize=12)
+plt.ylabel("yaxis",fontsize=12)
+plt.title("Vachan-1KI23CS173")
+plt.show()
