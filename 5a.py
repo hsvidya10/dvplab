fa14bf8158c5b2cf0f9fef6ddcc7952fb62f6398
hsvidya10 <vidyavidya1776@gmail.com>
2024-10-16 00:07:02 -0700

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist ([11,13,15,16,18,20,22,24,34,45,12,12],bins=4)
+plt.xlabel("Distribution of Age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("Vidhyalakshmi H S-1KI23CS177")
+plt.show()
