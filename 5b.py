48e0fa58c97278756a0ebca1d7216590bd317461
hsvidya10 <vidyavidya1776@gmail.com>
2024-10-16 00:07:36 -0700

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['Dester','Rangeover','Swift','Scorpio','kia','benz']
+data=[30,40,50,20,60,90]
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=(CARS))
+plt.title("Vidhyalakshmi H S-1KI23CS177")
+plt.show()
