dc6912bb9c45c5b45223d5c58fc1ae499bab78cc
hsvidya10 <vidyavidya1776@gmail.com>
2024-10-15 23:09:52 -0700

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['SSLC','PUC','1 sem','2sem']
+values=[500,600,650,690]
+plt.bar(std,values,color= 'green')
+plt.xlabel("Education",fontsize=12)
+plt.ylabel("Marks obtained",fontsize=12)
+plt.title("Vachan-1KI23CS173")
+plt.show()
