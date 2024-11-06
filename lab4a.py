6bfcf88dd4c389d9cd2f9342cfed2c6a61693176
hsvidya10 <vidyavidya1776@gmail.com>
2024-11-05 21:59:40 -0800

    Add files via upload

---
+++ lab4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['sslc','puc','1sem','2sem']
+values=[500,600,680,690]
+plt.bar(std,values,color='red')
+plt.xlabel("Education", fontsize=12)
+plt.ylabel("Marks obtained", fontsize=12)
+plt.title("Vidhyalakshmi H S-1KI23CS177")
+plt.show()
