c4b70edb6ed09a3350de5ba31398d393eb9c7be1
hsvidya10 <vidyavidya1776@gmail.com>
2024-11-05 23:04:44 -0800

    Add files via upload

---
+++ lab4bb.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3]
+plt.scatter(x,y,color='blue')
+plt.xlabel("x-axis",fontsize=12)
+plt.ylabel("y-axis",fontsize=12)
+plt.title("Vidhyalakshmi H S- 1KI23CS177")
+plt.show()
