3c70ddc5ed4537c87b8a2bc37f83103e9cbcf73c
hsvidya10 <vidyavidya1776@gmail.com>
2024-11-12 22:02:18 -0800

    Update 4a.py

--- 4a.py
+++ 4a.py
@@ -4,5 +4,5 @@ values=[500,600,650,690]
 plt.bar(std,values,color= 'green')
 plt.xlabel("Education",fontsize=12)
 plt.ylabel("Marks obtained",fontsize=12)
-plt.title("Vachan-1KI23CS173")
+plt.title("Vidhyalakshmi H S-1KI23CS177")
 plt.show()
